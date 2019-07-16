04e49a5c93c6f455b6796b3849885b699ee163c1
Nic Thibodeaux <dnt2111@gmail.com>
2019-07-16 11:58:18 -0400

    N/A

--- utils/BatchConvertFLIR.m
+++ utils/BatchConvertFLIR.m
@@ -1,20 +1,27 @@
-function BatchConvertFLIR(mouse,run,f_start,f_num,fr,pth)
-path = fullfile(findmousefolder(mouse),'webcam');
-webdir = fullfile(path,run);
-vidObj = VideoWriter(fullfile(pth,[mouse '_' run '_' mat2str(f_start) '-' mat2str(f_start+f_num) '.avi']));
+% Inputs:
+% mouse (string)
+% run (string)
+% frames to load (array)
+
+
+function LoadFLIR(mouse,run,vf,fr,savepth)
+mdir = findmousefolder(mouse);
+webdir = fullfile(mdir,'webcam',run);
+vidObj = VideoWriter(fullfile(savepth,[mouse '_' run '_' mat2str(vf(1)) '_' mat2str(numel(vf)) '.avi']));
 vidObj.FrameRate = fr;
 open(vidObj)
-vf = [1:f_num]+f_start;
 outwidth = 720;
 chk_sz = 1000;
-chk_st = 1:chk_sz:f_num;
+chk_st = 1:chk_sz:numel(vf);
 % write chk_sz frame chunks, then append to video file
 for c = 1:numel(chk_st)-1
     parfor k = 1:(chk_st(c+1)-chk_st(c))
-        w0 = LoadFLIR(webdir,vf(k+chk_st(c)-1),0,outwidth);
-        w1 = LoadFLIR(webdir,vf(k+chk_st(c)-1),1,outwidth);
+        idx = vf(k+chk_st(c)-1);
+        w0 = LoadFLIR(webdir,idx,0,outwidth);
+        w1 = LoadFLIR(webdir,idx,1,outwidth);
         tmp = cat(2,w0,w1);
-        txt = sprintf([mouse '_' run ' %0.1f sec'],round(vf(k+chk_st(c)-1)/fr));
+        tval = round(idx*10/fr)/10;
+        txt = sprintf([mouse '_' run ' %.1f sec'],tval);
         a(k).cdata = uint8(mean(insertText(tmp,[10 10],txt,'FontSize',18),3));
         a(k).colormap = gray(256);
     end
@@ -23,13 +30,16 @@ end
 clear a
 % Last chunk
 parfor k = 1:(numel(vf)-chk_st(end)+1)
-    w0 = LoadFLIR(webdir,vf(k+chk_st(end)-1),0,outwidth);
-    w1 = LoadFLIR(webdir,vf(k+chk_st(end)-1),1,outwidth);
+    idx = vf(k+chk_st(end)-1);
+    w0 = LoadFLIR(webdir,idx,0,outwidth);
+    w1 = LoadFLIR(webdir,idx,1,outwidth);
     tmp = cat(2,w0,w1);
-    txt = sprintf([mouse '_' run ' %0.1f sec'],vf(k)/fr);
+    tval = round(idx*10/fr)/10;
+    txt = sprintf([mouse '_' run ' %.1f sec'],tval);
     a(k).cdata = uint8(mean(insertText(tmp,[10 10],txt,'FontSize',18),3));
     a(k).colormap = gray(256);
 end
+disp('Almost done...')
 writeVideo(vidObj,a)
 clear a
 close(vidObj)
--- utils/findimportantpix.m
+++ utils/findimportantpix.m
@@ -1,14 +1,12 @@
 % Searches along timeseries data for pixels/voxels that exceed a threshold
 % a given number of times. Useful for extracting important regions from
 % highly sparse datasets.
-function binmask = findimportantpix(data,stdT,numT,info)
+function binmask = findimportantpix(data,stdT,numT,pbleach)
 binmask = zeros(1,size(data,1));
-fs = info.daq.scanRate;
 parfor i = 1:size(data,1)
     % Filter
-    TC = highpass(data(i,:),1/10,fs);
-    stdTC = std(TC);
-    if length(find(TC>stdTC*stdT)) > numT
+    stdTC = std(data(i,:));
+    if length(find(data(i,:) > stdTC * stdT)) > numT
         binmask(i) = 1;
     end
 end
